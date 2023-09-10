c0f790939287e6b337a85c5b654c645a8f8707e3
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2023-09-10 19:34:32 +0200

    choose folder

---
+++ src/lib/ChooseFolder.svelte
@@ -0,0 +1,35 @@
+<script>
+	let folderChosen = false;
+
+	function chooseFolder() {
+		// Here, you would typically open a directory chooser.
+		// For the sake of this example, we'll just simulate it.
+		folderChosen = true;
+	}
+</script>
+
+<div class="container">
+	<h1>Welcome</h1>
+	<p>Choose a directory to get started</p>
+	<button on:click={chooseFolder}>Choose Folder</button>
+
+	{#if folderChosen}
+		<button>Next</button>
+	{/if}
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		font-family: Arial, sans-serif;
+	}
+
+	button {
+		margin-top: 20px;
+		padding: 10px 20px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
+	import ChooseFolder from '$lib/ChooseFolder.svelte';
 	import Greet from '../lib/Greet.svelte';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <Greet />
+<ChooseFolder />
