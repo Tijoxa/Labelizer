55febaebaf32d039bece9f92301d0fb35236348a
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2023-09-15 00:42:27 +0200

    doc

--- src/lib/DisplayImages.svelte
+++ src/lib/DisplayImages.svelte
@@ -10,8 +10,11 @@
 	}
 </script>
 
+<!-- TODO: give file_path to backend, render image and send this with canva to draw on, and switch next previous with arrow key -->
+
 <div class="display">
 	{#each $imagePathsStore || [] as path (path)}
-		<src={pathToFileURL(path)} alt="Image from folder" />
+		<p1>{pathToFileURL(path)}</p1>
+		<!-- <img src={pathToFileURL(path)} alt="From folder" /> -->
 	{/each}
 </div>
